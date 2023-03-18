8eaca404fe09444655b1142648b1dc1929fa7d38
JoseArturoBadillo1 <128010173+JoseArturoBadillo1@users.noreply.github.com>
2023-03-18 14:26:51 -0600

    Add files via upload
    Commit 1

---
+++ CV_JoseArturoBadilloOlguin/js/js.js
@@ -0,0 +1,8 @@
+function boton(valor){
+    valor == 'enviar'?Swal.fire({title:'Gracias por registate', text:'Pronto me pondre en conctacto contigo'}): 
+    console('Error');
+  }
+  
+function alerta(){
+    alert("enviar datos");
+}
